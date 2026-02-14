aef4d94f8a18ebb3ac80d6c895dd431dad341578
Samihasan747 <1102samihasan@gmail.com>
2026-02-14 23:05:02 +0600

    Navbar Completed

---
+++ e-commerce-demo/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar
+        logo="icon111.png"
+        title="S-Mart"
+        home="Home"
+        Checkout="Checkout"
+        Username="Username"
+        Signup="Sign Up/Log In"
+      />
+    </>
+  );
+}
+
+export default App;
